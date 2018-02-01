d3a06d79a38b2ae26d1699d9d6ca3f3d8247d3d4
ExaHexa <block.lukas@gmx.de>
2018-02-01 17:39:38 +0100

    Add comments in AudioFile.java

--- src/com/github/exahexa/yukebox/data/AudioFile.java
+++ src/com/github/exahexa/yukebox/data/AudioFile.java
@@ -9,6 +9,10 @@ import java.util.Collection;
 /**
  * @author exahexa
  * 
+ * Abstract representation of an audio file. 
+ * The Class AudioFile is Serializable and implements (as leaf node)
+ * the MusicLibObj interface. It contains meta data from the audio file and 
+ * its path in the file system.
  * 
  */
 public class AudioFile implements Serializable, MusicLibObj{
@@ -28,11 +32,13 @@ public class AudioFile implements Serializable, MusicLibObj{
 	private String album;
 	
 	/**
-	 * @param title
-	 * @param track 
-	 * @param duration
-	 * @param filePath
-	 * @param fileName
+	 * Constructs a new AudioFile specified by the given arguments.
+	 * 
+	 * @param title the title of this AudioFile
+	 * @param trackNr the track number of this AudioFile
+	 * @param duration the duration of this AudioFile
+	 * @param filePath the system file path of this AudioFile
+	 * @param fileName the file name of this AudioFile
 	 */
 	public AudioFile(String name, String trackNr, float duration, String filePath,
 							String fileName, String artist, String album) {
@@ -47,61 +53,40 @@ public class AudioFile implements Serializable, MusicLibObj{
 			this.fileName = fileName;
 			this.artist = artist;
 			this.album = album;
-		}
-		else {
-			throw new IllegalArgumentException();
-		}
-		
+		}		
 	}
 	
-	/**
-	 * 
-	 * @return
-	 */
-	public String getName() {
-		return name;
-	}
+	
+
 	
 	/**
-	 * 
-	 * @param title
-	 */
-	public void setName(String name) {
-		if(name != null && !name.isEmpty()) {
-			this.name = name;
-		}
-		else {
-			throw new IllegalArgumentException();
-		}
-	}
-	
-	/**
-	 * 
-	 * @return
+	 * Returns a string that contains the track number of this specific AudioFile
+	 * @return the track number
 	 */
 	public String getTrackNr() {
 		return trackNr;
 	}
 
 	/**
-	 * 
-	 * @param track
+	 * Sets the track number of this specific AudioFile
+	 * @param trackNr a string that contains the track number 
 	 */
 	public void setTrackNr(String trackNr) {
 		this.trackNr = trackNr;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Returns the file path of this specific AudioFile on the file system
+	 * that contains the file
+	 * @return the path of the AudioFile
 	 */
 	public String getFilePath() {
 		return filePath;
 	}
 	
 	/**
-	 * 
-	 * @param filePath
+	 * Sets the path specified by the argument of the same name
+	 * @param filePath the path to set for this AudioFile
 	 */
 	public void setFilePath(String filePath) {
 		if(filePath != null && !filePath.isEmpty()) {
@@ -114,16 +99,16 @@ public class AudioFile implements Serializable, MusicLibObj{
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Returns the file name of this specific AudioFile
+	 * @return the file name
 	 */
 	public String getFileName() {
 		return fileName;
 	}
 	
 	/**
-	 * 
-	 * @param fileName
+	 * Sets the file name specified by the argument of the same name
+	 * @param fileName the name to set this AudioFile file name
 	 */
 	public void setFileName(String fileName) {
 		if(fileName != null && !fileName.isEmpty()) {
@@ -136,13 +121,17 @@ public class AudioFile implements Serializable, MusicLibObj{
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Returns the duration of this specific AudioFile as milliseconds
+	 * @return the duration as milliseconds
 	 */
 	public int getDuration() {
 		return (int)duration;
 	}
 	
+	/**
+	 * Returns a string that contains the duration in hh:mm:ss format
+	 * @return a string that contains the duration in hh:mm:ss format
+	 */
 	public String getDurationFormat() {
 		int sec = (int)(duration/1000)%60;
 		int min = (int)(duration/(60*1000))%60;
@@ -167,35 +156,39 @@ public class AudioFile implements Serializable, MusicLibObj{
 	}
 	
 	/**
-	 * @return the artistKey
+	 * Returns the name of the Artist that contains this AudioFile
+	 * @return the artist name
 	 */
 	public String getArtist() {
 		return artist;
 	}
 
 	/**
-	 * @param artistKey the artistKey to set
+	 * Sets the artistKey of this AudioFile specified by the argument artist
+	 * @param artist the name of artist to set the artist that contains this AudioFile
 	 */
 	public void setArtistKey(String artist) {
 		this.artist = artist;
 	}
 
 	/**
-	 * @return the albumKey
+	 * Returns the name of the Album that contains this AudioFile 
+	 * @return the album name
 	 */
 	public String getAlbum() {
 		return album;
 	}
 
 	/**
-	 * @param albumKey the albumKey to set
+	 * Sets the name of the Album that contains this AudioFile 
+	 * @param album specifies the name of the Album that contains this AudioFile
 	 */
-	public void setAlbumKey(String album) {
+	public void setAlbum(String album) {
 		this.album = album;
 	}
 	
 	/**
-	 * @see 
+	 * @see Object#equals(Object)
 	 */
 	@Override
 	public boolean equals(Object obj) {
@@ -207,40 +200,75 @@ public class AudioFile implements Serializable, MusicLibObj{
 				&& (this.duration == ((AudioFile)obj).getDuration() );
 	}
 
+	/**
+	 * @see MusicLibObj#getName()
+	 */
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	 * @see MusicLibObj#setName(String) 
+	 */
+	@Override
+	public void setName(String name) {
+		if(name != null && !name.isEmpty()) {
+			this.name = name;
+		}
+	}
+
+	/**
+	 * @see MusicLibObj#add(MusicLibObj)
+	 */
 	@Override
 	public void add(MusicLibObj e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see MusicLibObj#delete(MusicLibObj)
+	 */
 	@Override
 	public void delete(MusicLibObj e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * @see MusicLibObj#contains(MusicLibObj)
+	 */
 	@Override
 	public boolean contains(MusicLibObj e) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * @see MusicLibObj#containsKey(String)
+	 */
 	@Override
 	public boolean containsKey(String key) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public Collection<MusicLibObj> getValues() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+	/**
+	 * @see MusicLibObj#isLeaf()
+	 */
 	@Override
 	public boolean isLeaf() {
 		return true;
 	}
-	
+
+	/**
+	 * @see MusicLibObj#getValues()
+	 */
+	@Override
+	public Collection<MusicLibObj> getValues() {
+		return null;
+	}
 
 }
