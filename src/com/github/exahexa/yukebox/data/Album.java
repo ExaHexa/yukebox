182a847be04652a6d6801f99ade7e97b87f3077e
ExaHexa <block.lukas@gmx.de>
2018-02-01 18:04:16 +0100

    Add comments in Album.java

--- src/com/github/exahexa/yukebox/data/Album.java
+++ src/com/github/exahexa/yukebox/data/Album.java
@@ -10,7 +10,12 @@ import com.google.common.collect.HashMultimap;
 
 /**
  * @author exahexa
- *
+ * 
+ * Abstract representation of an album. The class implements Serializable 
+ * and MusicLibObj. Album contains the album name, the optional release data, 
+ * a MusicLibObj multimap of its track and the name of the artist that 
+ * contains this album.
+ * 
  */
 public class Album implements Serializable, MusicLibObj{
 	
@@ -27,6 +32,12 @@ public class Album implements Serializable, MusicLibObj{
 	
 	//private int initCapacity = 19;
 	
+	/**
+	 * Constructs a new Album specified by the arguments name and artist
+	 * @param name the name of this Album
+	 * @param artist the name of the Artist that contains this Album
+	 * 
+	 */
 	public Album(String name, String artist) {
 		if(name != null && !name.isEmpty() 
 						&& artist != null && !artist.isEmpty()) {
@@ -40,8 +51,10 @@ public class Album implements Serializable, MusicLibObj{
 	}
 	
 	/**
-	 * @param name
-	 * @param releaseData
+	 * Constructs a new Album specified by the given arguments
+	 * @param name the name of this Album
+	 * @param artist the name of the Artist that contains this Album
+	 * @param releaseDate the release data of this Album
 	 */
 	public Album(String name, String artist, int releaseDate) {
 		if(name != null && !name.isEmpty()
@@ -57,51 +70,60 @@ public class Album implements Serializable, MusicLibObj{
 		this.tracks = HashMultimap.create();
 	}
 	
+	/**
+	 * @see MusicLibObj#add(MusicLibObj)
+	 */
 	@Override
 	public void add(MusicLibObj e) {
-		if(e != null) {
-			if(!contains(e)) {
+		if(e != null && !contains(e)) {
 				this.tracks.put(e.getName().toLowerCase(), e);
-			}
-			else {
-				throw new ElementAlreadyExistsException();
-			}
-			
-		}
-		else {
-			throw new IllegalArgumentException();
 		}
 		
 	}
-
+	
+	/**
+	 * @see MusicLibObj#delete(MusicLibObj)
+	 */
 	@Override
 	public void delete(MusicLibObj e) {
 		tracks.remove(e.getName().toLowerCase(), e);
 		
 	}
-
+	
+	/**
+	 * @see MusicLibObj#contains(MusicLibObj)
+	 */
 	@Override
 	public boolean contains(MusicLibObj e) {
 		return tracks.containsEntry(e.getName().toLowerCase(), e);
 	}
-
+	
+	/**
+	 * @see MusicLibObj#containsKey(String)
+	 */
 	@Override
 	public boolean containsKey(String key) {
 		return !key.isEmpty() && tracks.containsKey(key.toLowerCase());
 	}
 	
+	/**
+	 * @see MusicLibObj#isLeaf()
+	 */
 	@Override
 	public boolean isLeaf() {
 		return false;
 	}
-
+	
+	/**
+	 * @see MusicLibObj#getValues()
+	 */
 	@Override
 	public Collection<MusicLibObj> getValues() {
 		return tracks.values();
 	}
+	
 	/**
-	 * 
-	 * @return
+	 * @see MusicLibObj#getName()
 	 */
 	@Override
 	public String getName() {
@@ -109,8 +131,7 @@ public class Album implements Serializable, MusicLibObj{
 	}
 	
 	/**
-	 * 
-	 * @param name
+	 * @see MusicLibObj#setName(String)
 	 */
 	@Override
 	public void setName(String name) {
@@ -126,35 +147,39 @@ public class Album implements Serializable, MusicLibObj{
 	}
 	
 	/**
-	 * @return the releaseDate
+	 * Returns the release date of this Album
+	 * @return the release date of this Album
 	 */
 	public int getReleaseDate() {
 		return releaseDate;
 	}
 
 	/**
-	 * @param releaseDate the releaseDate to set
+	 * Sets the release date of this Album
+	 * @param releaseDate the release date to set 
 	 */
 	public void setReleaseDate(int releaseDate) {
 		this.releaseDate = releaseDate;
 	}
 
 	/**
-	 * @return the artistKey
+	 * Returns the name of the Artist that contains this Album
+	 * @return the name of the Artist that contains this Album
 	 */
 	public String getArtist() {
 		return artist;
 	}
 
 	/**
-	 * @param artistKey the artistKey to set
+	 * Sets the name of the Artist that contains this Album
+	 * @param artist the name of the artist to set
 	 */
-	public void setArtist(String artistKey) {
-		this.artist = artistKey;
+	public void setArtist(String artist) {
+		this.artist = artist;
 	}
 	
 	/**
-	 * @see 
+	 * @see Object#equals(Object)
 	 */
 	@Override
 	public boolean equals(Object obj) {
