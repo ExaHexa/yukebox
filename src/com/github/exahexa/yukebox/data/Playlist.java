16745ab6a247bcf3a3b5f87167ee3edaafee6c81
ExaHexa <block.lukas@gmx.de>
2018-02-05 09:51:01 +0100

    Add comments in Playlist.java

--- src/com/github/exahexa/yukebox/data/Playlist.java
+++ src/com/github/exahexa/yukebox/data/Playlist.java
@@ -24,7 +24,9 @@ public class Playlist implements Serializable{
 	private int indexLastPlayed;
 
 	/**
-	 * 
+	 * Constructs a new Playlist whose name is specified by the argument 
+	 * of the same name
+	 * @param name 
 	 */
 	public Playlist(String name) {
 		this.name = name;
@@ -32,34 +34,68 @@ public class Playlist implements Serializable{
 		indexLastPlayed = 0;
 	}
 	
+	/**
+	 * Returns this Playlist name
+	 * @return this Playlist name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Sets this Playlist name
+	 * @param name 
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Adds an element to the list, specified by the argument track 
+	 * @param track the MusicLibObj to add
+	 */
 	public void addTrack(MusicLibObj track) {
 		playlist.add(track);
 	}
 	
+	/**
+	 * Adds a MusicLibObj type collection to the list, specified
+	 * by the argument collectionTrack
+	 * @param collectionTrack the collection to add
+	 */
 	public void addCollection(Collection<MusicLibObj> collectionTrack) {
 		playlist.addAll(collectionTrack);
 	}
 	
+	/**
+	 * Returns the number of elements in this Playlist
+	 * @return the number of elements in this Playlist
+	 */
 	public int getPlaylistSize() {
 		return playlist.size();
 	}
 	
+	/**
+	 * Returns the index of the last played element
+	 * @return the index of the last played element 
+	 */
 	public int getIndexLastPlayed() {
 		return indexLastPlayed;
 	}
 	
+	/**
+	 * Returns the next element 
+	 * @return the next element
+	 */
 	public MusicLibObj getNext() {
 		return getAudioFileByIndex((indexLastPlayed +1));
 	}
 	
+	/**
+	 * Returns the element at the specified position in this list
+	 * @param index index of the element to return
+	 * @return the element at the specified position in this list
+	 */
 	public MusicLibObj getAudioFileByIndex(int index) {
 		if(index < playlist.size()) {
 			indexLastPlayed = index;
@@ -71,6 +107,10 @@ public class Playlist implements Serializable{
 		}
 	}
 	
+	/**
+	 * Returns an ArrayList that contains MusicLibObj in this Playlist
+	 * @return
+	 */
 	public ArrayList<MusicLibObj> getPlaylist(){
 		return playlist;
 	}
